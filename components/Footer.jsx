7bfeb398260844c4ab597913ebcf475df046775c
md areeb uddin <mdareeb.uddin@truelancer.com>
2022-12-06 20:37:45 +0530

    Navbar and footer are ready

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+export default function Footer () {
+    return (
+        <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.200">
+            2022 Sky Estate, Inc.
+        </Box>
+    )
+}
